5b737e7cfe50a9676033978dd44dc18591628cb5
akshay <akshaymk94@github.com>
2018-02-09 14:09:08 +0530

    numerical module is being developed. rendering content from an array onto browser has issues

--- js/script.js
+++ js/script.js
@@ -1,198 +1,145 @@
 $(document).ready(function() {
-    var autoFillContent;
-    var titles=[];
-    var tagsText = "";
-  
-    function questionRenderer() {
-        $.getJSON('json/qui.json', function(data) {
     
-            var i = 0;
-            $.each(data, function(index, info) {
-                var tempo = data.qns[i]["sasts"];
-                $.each(tempo, function(index, value) {
-                    tagsText += '<span class="tags">' + value["title"] + '<span collect-id="'+ value["id"] +'" class="glyphicon glyphicon-remove"></span>\
-                                    </span>'
-                });
-                
-                var html_text = '<div class="box">\
-                                    <div class="boxItem1">\
-                                        <div class="questionContainer">\
-                                            <header>\
-                                                <label id="boxLabel'+(i+1)+'">' + data.qns[i].u_id + '</label>\
-                                            </header>\
-                                            <p class="lead" id="lead'+(i+1)+'">' + data.qns[i].content + '</p>\
-                                            <div class="choiceContainer">\
-                                                <form class="formChoices">\
-                                                    <input type="radio" id="radioFirstChoice" name="choice" class="choiceLeft" value="1stradiovalue">hello\
-                                                    <input type="radio" id="radioSecondChoice" name="choice" class="choiceRight" value="2ndradiovalue">hello\
-                                                    <input type="radio" id="radioThirdChoice" name="choice" class="choiceLeft" value="3rdradiovalue">hello\
-                                                    <input type="radio" id="radioFourthChoice" name="choice" class="choiceRight" value="4thradiovalue">hello\
-                                                </form>\
-                                            </div>\
-                                            <footer id="foot"> '+ tagsText +'\
-                                            </footer>\
-                                        </div>\
-                                        <div class="formSubmitBox">\
-                                            <input type="text"  class="inputAutoFill">\
-                                            <button type="button" class="btnSubmit">submit</button>\
-                                        </div>\
-                                    </div>\
-                                </div>';
-                $('#main').append(html_text);
-                i++;
-                tagsText = "";
-            });
+    var num = 0;
+    var question = 0;
+    var answer = 0;
+    var explanation = 0;
+    var textarea = 0;
+    var edit = 0;
+    
+    var saveQuestions=[];
+    
+    
+    
+    function getQuestions() {
+        
+        
+        $.getJSON('json/info.json', function(data) {
+            
+            var tempo = data["question_details"]; 
+
+            for(var i = 0; i < tempo.length; i++) {
+                var obj = {}
+                obj["qn_id"] = tempo[i]["qn_id"]
+                obj["qn_content"] = tempo[i]["qn_content"]
+                obj["ans_id"] = tempo[i]["ans_id"]
+                obj["ans_content"] = tempo[i]["ans_content"]
+                obj["soln_id"] = tempo[i]["soln_id"]
+                obj["soln_content"] = tempo[i]["soln_content"]
+                saveQuestions.push(obj);
+            }
         });
     }
     
-    $('body').on('click','.glyphicon-remove', function() {
-        var collectedId = $(this).attr('collect-id');
-        alert(collectedId);
-    });
+    getQuestions();
+    
+    function questionRenderer() {
+        
+        $.each(saveQuestions, function(index, value) {
+                var mynewbox = '<div class="container box" id="box'+(num + 1)+'">\
+                                    <div class="glyphicon glyphicon-remove" toRemove="box'+(num + 1)+'"></div>\
+                                    <div class="rendererBox">\
+                                        <div class="questionRenderer questionContent" id="question'+(question+1)+'">\
+                                            <span class="iconAdd">&plus;</span>\
+                                            <strong class="question" parentId="question'+(question+1)+'">Question</strong>\
+                                        </div>\
+                                        <div class="answerRenderer questionContent" id="answer'+(answer+1)+'">\
+                                            <span class="iconAdd">&plus;</span>\
+                                            <strong class="answer" parentId="answer'+(answer+1)+'">Answer</strong>\
+                                        </div>\
+                                        <div class="explanationRenderer questionContent" id="explanation'+(explanation+1)+'">\
+                                            <span class="iconAdd">&plus;</span>\
+                                            <strong class="explanation" parentId="explanation'+(explanation+1)+'">Explanation</strong>\
+                                        </div>\
+                                    </div>\
+                                </div>';
+
+                $('#QandAContainer').append(mynewbox);
+
+                num++;
+                question++;
+                answer++;
+                explanation++;
+            });
+        }
     
     questionRenderer();
-        
-    function getSastTags() {
-        $.getJSON('json/sasts.json', function(autoFillContent) {
-        var temp = autoFillContent["sast_ids"];
-        
-        for(var ind = 0; ind < temp.length; ind++) {
-            var obj = {}
-            obj["label"] = temp[ind]["title"]
-            obj["id"] = temp[ind]["id"]
-            obj["value"] = temp[ind]["title"]
-            titles.push(obj);
-        }
-         }); 
-    }
     
-    getSastTags();
-      
-    //autocomplete the input on click
-    $('body').on('click','.inputAutoFill',function(){
-        $(this).autocomplete({
-            source: titles,
-            select:function(event,ui){
-                alert(ui.item.id)
-                
-            }
-        });        
-    })
     
-    //submit the selected tag to backend
-    $('body').on('click','.btnSubmit', function() {
-        var gifLoad = '<img class="gifLoading" src="img/newload.gif" height="20px" width="20px">';
-        $(this).append(gifLoad);
+    
+    $('body').on('click','#addQandABox', function() {
+        var QandABox = '<div class="container box" id="box'+(num + 1)+'">\
+                            <div class="glyphicon glyphicon-remove" toRemove="box'+(num + 1)+'"></div>\
+                            <div class="rendererBox">\
+                                <div class="questionRenderer questionContent" id="question'+(question+1)+'">\
+                                    <span class="iconAdd">&plus;</span>\
+                                    <strong class="question" parentId="question'+(question+1)+'">Question</strong>\
+                                </div>\
+                                <div class="answerRenderer questionContent hidden" id="answer'+(answer+1)+'">\
+                                    <span class="iconAdd">&plus;</span>\
+                                    <strong class="answer" parentId="answer'+(answer+1)+'">Answer</strong>\
+                                </div>\
+                                <div class="explanationRenderer questionContent hidden" id="explanation'+(explanation+1)+'">\
+                                    <span class="iconAdd">&plus;</span>\
+                                    <strong class="explanation" parentId="explanation'+(explanation+1)+'">Explanation</strong>\
+                                </div>\
+                            </div>\
+                        </div>';
+        $('#QandAContainer').prepend(QandABox);
         
-        setTimeout(function() {
-            $('.gifLoading').remove();
-            setTimeout(function() {
-                $.notify("Update Successful!","success");
-            },100);
-        }, 3000);
-        
-        
+        num++;
+        question++;
+        answer++;
+        explanation++;
     });
     
-    //trigger this function by clicking on any question to display its contents in right container 
-    $('body').on('click','.lead', function() {
-        var oldQuestion = $(this).html();
-        var questionId = $(this).attr("id");
-        $('#questionEditor').attr('questionFrom',questionId);
-        $('#questionEditor').html(oldQuestion);
-        
-        var oldOption1 = $(this).next().find("#radioFirstChoice").val();
-        var radioOption1Id = $(this).next().find("#radioFirstChoice").attr("id");
-        $('#txtFirstChoice').attr('choiceFrom', radioOption1Id);
-        $('#txtFirstChoice').html(oldOption1);
-        
-        var oldOption2 = $(this).next().find("#radioSecondChoice").val();
-        var radioOption2Id = $(this).next().find("#radioSecondChoice").attr("id");
-        $('#txtSecondChoice').attr('choiceFrom', radioOption2Id);
-        $('#txtSecondChoice').html(oldOption2);
-        
-        var oldOption3 = $(this).next().find("#radioThirdChoice").val();
-        var radioOption3Id = $(this).next().find("#radioThirdChoice").attr("id");
-        $('#txtThirdChoice').attr('choiceFrom', radioOption3Id);
-        $('#txtThirdChoice').html(oldOption3);
-        
-        var oldOption4 = $(this).next().find("#radioFourthChoice").val();
-        var radioOption4Id = $(this).next().find("#radioFourthChoice").attr("id");
-        $('#txtFourthChoice').attr('choiceFrom', radioOption4Id);
-        $('#txtFourthChoice').html(oldOption4);
-        
-        $('.formEditBox').removeClass('hidden').css('border','solid 1px #aaa');
-        
-        $('.boxItem2').removeClass('hidden');
-        $('#btnSubmitEditedQuestion').hide();
-        $('.boxItem2').show();
-        $('.ckeditor').hide();
-        
-        
+    $('body').on('click','.glyphicon-remove', function() {
+        var elementToRemove = $(this).attr('toRemove');
+        $("#" + elementToRemove + "").remove();
     });
     
-    
-    //submit the edited question, options back to left container
-    $('body').on('click','#btnSubmitEditedQuestion', function() {
-        var editedQuestion = $('#questionEditor').html();
-        var editedChoice1 = $('#txtFirstChoice').val();
-        var editedChoice2 = $('#txtSecondChoice').val();
-        var editedChoice3 = $('#txtThirdChoice').val();
-        var editedChoice4 = $('#txtFourthChoice').val();
-        var returnEditedQuestionTo = $('#questionEditor').attr("questionFrom");
-        var returnChoice1To = $('#txtFirstChoice').attr("choiceFrom");
-        var returnChoice2To = $('#txtSecondChoice').attr("choiceFrom");
-        var returnChoice3To = $('#txtThirdChoice').attr("choiceFrom");
-        var returnChoice4To = $('#txtFourthChoice').attr("choiceFrom");
-        $("#" +returnEditedQuestionTo+ "").html(editedQuestion);
-        $("#" +returnChoice1To + "").val(editedChoice1);
-        $("#" +returnChoice2To + "").val(editedChoice2);
-        $("#" +returnChoice3To + "").val(editedChoice3);
-        $("#" +returnChoice4To + "").val(editedChoice4);
-        $('#questionEditor').html("");
-        $('#txtFirstChoice').val("");
-        $('#txtSecondChoice').val("");
-        $('#txtThirdChoice').val("");
-        $('#txtFourthChoice').val("");
-        $('.boxItem2').hide();
+    $('body').on('click','.questionContent strong', function() {
+        var editableText = $(this).html();
+        $(this).hide();
+        var sendBackTo = $(this).attr('parentId');
+        var replaceElement = '<div id="editor'+(edit + 1)+'">\
+                                    <textarea id="editArea'+(textarea+1)+'"  rows="5" cols="30">' + editableText + '</textarea>\
+                                    <button type="button" class="btn btn-success btnApply" textAreaId="editArea'+(textarea+1)+'" editorId="editor'+(edit + 1)+'" sendBackTo="'+ sendBackTo +'">Apply</button>\
+                                    <button type="button" class="btn btn-warning btnCancel" editorId="editor'+(edit + 1)+'" sendBackTo="'+ sendBackTo +'">Cancel</button>\
+                              </div>';
         
-    });
-    
-    CKEDITOR.replace( 'editor', {
+        $("#" + sendBackTo + "").append(replaceElement);
+        $("#" + sendBackTo + "").find('.iconAdd').hide();
+        $(this).hide();
+        
+        CKEDITOR.replace( "editArea"+(textarea+1)+"", {
 			extraPlugins: 'mathjax',
 			mathJaxLib: 'http://cdn.mathjax.org/mathjax/2.6-latest/MathJax.js?config=TeX-AMS_HTML',
 			height: 320,
 			filebrowserUploadUrl: '/ckeditor/img_upload/'
-		} );
-    
-    //trigger this function by clicking on the question/options on right container to edit them
-    $('body').on('click','.rightContainerContent', function() {
-        var toEdit = $(this).html();
-        var returnId = $(this).attr('id');
-        $('#editor').attr('sendBackTo', returnId);
-        $('.formEditBox').hide();
-        $('#editor').val(toEdit);
-        $('.ckeditor').show();
-    });
-    
-    //save the updated question/option on the right container
-    $('body').on('click','#applyUpdate', function() {
-        var updatedText = CKEDITOR.instances.editor.getData();
-        var returnUpdateTo = $('#editor').attr('sendBackTo');
-        $("#" + returnUpdateTo + "").html(updatedText);
-        var ex = $("#" + returnUpdateTo + "").html();
-        alert(ex);
-        $('.ckeditor').hide();
-        $('#btnSubmitEditedQuestion').show();
-        $('.formEditBox').show();
-        updatedText = "";
-    });
-    
-    //cancel the update task on right container
-    $('body').on('click','#cancelUpdate', function() {
-        $('.ckeditor').hide();
+		});
         
-        $('.formEditBox').show();
+        
+        textarea++;
+        edit++;
     });
+    
+    $('body').on('click','.btnApply', function() {
+        var sendBackTo = $(this).attr('sendBackTo');
+        var textareaId = $(this).attr('textAreaId');
+        var editorId = $(this).attr('editorId');
+        var editedText =  CKEDITOR.instances.editor.getData();
+        $("#" + sendBackTo + "").find('strong').html(editedText).show();
+        $("#" + editorId + "").remove();
+    });
+    
+    $('body').on('click','.btnCancel', function() {
+        var editorId = $(this).attr('editorId');
+        var sendBackTo = $(this).attr('sendBackTo');
+        $("#" + sendBackTo + "").find('strong').show();
+        $("#" + editorId + "").remove();
+    });
+    
+    
     
 });
