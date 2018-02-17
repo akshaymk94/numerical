85b6aee076b79f17033ef70fe14a36367637cf1c
akshay <akshaymk94@github.com>
2018-02-17 12:09:47 +0530

    "chosen dropdown successfully added to numericals "

--- js/script.js
+++ js/script.js
@@ -11,9 +11,7 @@ $(document).ready(function() {
     var tagSelector = 0;
     var submitTags = 0;
     var saveQuestions=[];
-    
     var selectId = "";
-    
     var qn_id = 0;
     var qn_content = "";
     var ans_id = 0;
@@ -23,6 +21,8 @@ $(document).ready(function() {
     var tags = [];
     var preselectedTags = [];
     
+    getTags();
+
     function getQuestions() {
         $.getJSON('../json/info.json', function(data) {
             var tempo = data["question_details"]; 
@@ -38,12 +38,10 @@ $(document).ready(function() {
                 saveQuestions.push(obj);
             }
             boxRenderer();
-            
         });
     }
     
     getQuestions();
-    getTags();
     
     function getTags() {
         $.getJSON('../json/tags.json', function(tagsList) {
@@ -54,40 +52,8 @@ $(document).ready(function() {
                 obj["name"] = temp[i]["name"]
                 tags.push(obj);
             }
-            populateChosen();
-            //convertSelectToChosen();
-           
         });
-        
-        
     }
-    
-    function populateChosen() {
-        //alert("inside populate");
-        //$('.tagSelector').append('<option></option>');
-        $.each(tags, function(index,value) {
-            var newOption = '<option id="'+ value["id"] +'">'+ value["name"] +'</option>';
-            $('.tagSelector').append(newOption);/*.trigger("chosen:updated");*/
-        });
-        //convertSelectToChosen();
-    }
-    
-    function convertSelectToChosen(rootBox) {
-        
-        
-                //alert("inside convert");
-                $("#"+ rootBox +"").find('.tagSelector').chosen({
-                    width: "20%",
-                    no_results_text: "oops! search failed!",
-                    allow_single_deselect: true
-                });
-      ;
-        
-    }
-    
-     
-    
-    
     
     function boxRenderer() {
         $.each(saveQuestions, function(index, value) {
@@ -97,10 +63,7 @@ $(document).ready(function() {
             ans_content = value["ans_content"] == "" ? "Answer" : value["ans_content"];
             soln_id = value["soln_id"];
             soln_content = value["soln_content"] == "" ? "Explanation" : value["soln_content"];
-            
-            myNewFunction(qn_id, qn_content, ans_id, ans_content, soln_id, soln_content, tags, preselectedTags);
-            
-            
+            constructQuestionBox(qn_id, qn_content, ans_id, ans_content, soln_id, soln_content, tags, preselectedTags);
             qn_id = 0;
             qn_content = "";
             ans_id = 0;
@@ -110,86 +73,80 @@ $(document).ready(function() {
         });
     }
     
-    function myNewFunction(qn_id, qn_content, ans_id, ans_content, soln_id, soln_content) {
-            var mynewbox = '<div class="container box" id="box'+(num + 1)+'">\
-                                <label class="lblquestionId">' + qn_id + '</label>\
-                                <div class="glyphicon glyphicon-remove" toRemove="box'+(num + 1)+'" questionToRemoveId="' + qn_id + '"></div><br>\
-                                <div class="rendererBox">\
-                                    <div class="questionRenderer questionContent" id="question'+(question+1)+'" boxId="box'+(num + 1)+'">\
-                                        <span class="iconAdd">&plus;</span>\
-                                        <strong class="question" parentId="question'+(question+1)+'" questionId="'+qn_id+'" elementId="'+qn_id+'" boxId="box'+(num + 1)+'">'+ qn_content +'</strong>\
-                                    </div>\
-                                    <div class="answerRenderer questionContent" id="answer'+(answer+1)+'" boxId="box'+(num + 1)+'">\
-                                        <span class="iconAdd">&plus;</span>\
-                                        <strong class="answer" parentId="answer'+(answer+1)+'" questionId="'+qn_id+'" elementId="'+ ans_id +'" boxId="box'+(num + 1)+'">'+ ans_content +'</strong>\
-                                    </div>\
-                                    <button type="button" id="btnSolution'+(btnSolution+1)+'" class="btn btn-info btnSolution" explanationId="explanation'+(explanation+1)+'" boxId="box'+(num + 1)+'">view explanation</button>\
-                                    <div class="explanationRenderer questionContent" id="explanation'+(explanation+1)+'" boxId="box'+(num + 1)+'">\
-                                        <span class="iconAdd">&plus;</span>\
-                                        <strong class="explanation" parentId="explanation'+(explanation+1)+'" questionId="'+qn_id+'" elementId="'+ soln_id +'" boxId="box'+(num + 1)+'">'+ soln_content +'</strong>\
-                                    </div><br>\
-                                    <select id="tagSelector'+(tagSelector+1)+'" multiple class="tagSelector" boxId="box'+(num + 1)+'" questionId="'+qn_id+'"></select>\
-                                    <button id="submitTags'+ (submitTags+1) +'" type="button" selectId="tagSelector'+(tagSelector+1)+'" class="btn btn-success btnSubmitTags" boxId="box'+(num + 1)+'">Submit Tags</button>\
+    function constructQuestionBox(qn_id, qn_content, ans_id, ans_content, soln_id, soln_content) {
+        var mynewbox = '<div class="container box" id="box'+(num + 1)+'">\
+                            <label class="lblquestionId">' + qn_id + '</label>\
+                            <div class="glyphicon glyphicon-remove" toRemove="box'+(num + 1)+'" questionToRemoveId="' + qn_id + '"></div><br>\
+                            <div class="rendererBox">\
+                                <div class="questionRenderer questionContent" id="question'+(question+1)+'" boxId="box'+(num + 1)+'">\
+                                    <span class="iconAdd">&plus;</span>\
+                                    <strong class="question" parentId="question'+(question+1)+'" questionId="'+qn_id+'" elementId="'+qn_id+'" boxId="box'+(num + 1)+'">'+ qn_content +'</strong>\
                                 </div>\
-                            </div>';
-        
-            
-            $('#QandAContainer').prepend(mynewbox);
-           // alert("constructed box");
-            
-            
-            
-           
-            
-                var boxId = $("#question"+(question+1)+"").attr('boxId');
-                
-                if($("#question"+(question+1)+"").find('.question').html() == "Question") {
-                    $("#question"+(question+1)+"").siblings().hide();
-                } 
-                else
-                {
-                    $("#question"+(question+1)+"").find('.iconAdd').hide();
-                    if($("#answer"+(answer+1)+"").find('.answer').html() == "Answer") {
-                        $("#answer"+(answer+1)+"").show();
-                        $("#"+ boxId +"").find('.btnSolution').hide();
-                        $("#"+ boxId +"").find('.explanationRenderer').hide();
-                        $("#"+ boxId +"").find('.tagSelector').hide();
-                        $("#"+ boxId +"").find('.btnSubmitTags').hide();
-                        
-                    } 
-                    else {
-                        $("#answer"+(answer+1)+"").find('.iconAdd').hide();
-                        if ($("#explanation"+(explanation+1)+"").find('.explanation').html() == "Explanation") { 
-                            $("#explanation"+(explanation+1)+"").show();
-                            $("#"+ boxId +"").find('.btnSolution').hide();
-                            
-                            $("#"+ boxId +"").find('.tagSelector').hide();
-                            $("#"+ boxId +"").find('.btnSubmitTags').hide();
-                        
-                        }
-                        else {
-                            
-                            $("#explanation"+(explanation+1)+"").find('.iconAdd').hide();
-                            $("#"+ boxId +"").find('.btnSubmitTags').attr('disabled',true);
-                            //$("#"+ boxId +"").find('.btnSolution').show();
-                            //$("#"+ boxId +"").find('.tagSelector').hide();
-                            
-                            //$("#"+ boxId +"").find('.btnSubmitTags').hide();
-                            
-                            
-                        } 
-                    }
+                                <div class="answerRenderer questionContent" id="answer'+(answer+1)+'" boxId="box'+(num + 1)+'">\
+                                    <span class="iconAdd">&plus;</span>\
+                                    <strong class="answer" parentId="answer'+(answer+1)+'" questionId="'+qn_id+'" elementId="'+ ans_id +'" boxId="box'+(num + 1)+'">'+ ans_content +'</strong>\
+                                </div>\
+                                <button type="button" id="btnSolution'+(btnSolution+1)+'" class="btn btn-info btnSolution" explanationId="explanation'+(explanation+1)+'" boxId="box'+(num + 1)+'">view explanation</button>\
+                                <div class="explanationRenderer questionContent" id="explanation'+(explanation+1)+'" boxId="box'+(num + 1)+'">\
+                                    <span class="iconAdd">&plus;</span>\
+                                    <strong class="explanation" parentId="explanation'+(explanation+1)+'" questionId="'+qn_id+'" elementId="'+ soln_id +'" boxId="box'+(num + 1)+'">'+ soln_content +'</strong>\
+                                </div><br>\
+                                <select id="tagSelector'+(tagSelector+1)+'" multiple class="tagSelector" boxId="box'+(num + 1)+'" questionId="'+qn_id+'"></select>\
+                                <button id="submitTags'+ (submitTags+1) +'" type="button" selectId="tagSelector'+(tagSelector+1)+'" class="btn btn-success btnSubmitTags" boxId="box'+(num + 1)+'">Submit Tags</button>\
+                            </div>\
+                        </div>';
+
+        $('#QandAContainer').prepend(mynewbox);
+
+        $.each(tags, function(index,value) {
+            var newOption = '<option id="'+ value["id"] +'">'+ value["name"] +'</option>';
+            $("#tagSelector"+(tagSelector+1)+"").append(newOption).trigger("chosen:updated");
+        });
+
+        var boxId = $("#question"+(question+1)+"").attr('boxId');
+        if($("#question"+(question+1)+"").find('.question').html() == "Question") {
+            $("#question"+(question+1)+"").siblings().hide();
+        } 
+        else
+        {
+            $("#question"+(question+1)+"").find('.iconAdd').hide();
+            if($("#answer"+(answer+1)+"").find('.answer').html() == "Answer") {
+                $("#answer"+(answer+1)+"").show();
+                $("#"+ boxId +"").find('.btnSolution').hide();
+                $("#"+ boxId +"").find('.explanationRenderer').hide();
+                $("#"+ boxId +"").find('.tagSelector').hide();
+                $("#"+ boxId +"").find('.btnSubmitTags').hide();
+
+            } 
+            else {
+                $("#answer"+(answer+1)+"").find('.iconAdd').hide();
+                if ($("#explanation"+(explanation+1)+"").find('.explanation').html() == "Explanation") { 
+                    $("#explanation"+(explanation+1)+"").show();
+                    $("#"+ boxId +"").find('.btnSolution').hide();
+
+                    $("#"+ boxId +"").find('.tagSelector').hide();
+                    $("#"+ boxId +"").find('.btnSubmitTags').hide();
+
                 }
-                   
-                num++;
-                question++;
-                answer++;
-                explanation++;
-                btnSolution++;
-                tagSelector++;
-                submitTags++
+                else {
+                    $("#explanation"+(explanation+1)+"").find('.iconAdd').hide();
+                    $("#tagSelector"+(tagSelector+1)+"").chosen({
+                        width: "30%",
+                        no_results_text: "oops! search failed!",
+                        allow_single_deselect: true
+                    });
+                    $("#"+ boxId +"").find('.btnSubmitTags').attr('disabled',true);
+                } 
+            }
+        }
+        num++;
+        question++;
+        answer++;
+        explanation++;
+        btnSolution++;
+        tagSelector++;
+        submitTags++;
     }
-    
     
     $('body').on('click','#addQandABox', function() {
         qn_id = 0;
@@ -198,19 +155,11 @@ $(document).ready(function() {
         ans_content = "Answer";
         soln_id = 0;
         soln_content = "Explanation";
-        /*tags = "no tags!";
-        preselectedTags = "no preselected tags!";*/
-        
-        myNewFunction(qn_id, qn_content, ans_id, ans_content, soln_id, soln_content);
-        populateChosen();
-        
-       
+        constructQuestionBox(qn_id, qn_content, ans_id, ans_content, soln_id, soln_content);
         $(this).attr('disabled', true);
         $('html, body').animate({scrollTop : 0},200);
 		return false;
     });
-    
-    
     
     $('body').on('click','.glyphicon-remove', function() {
         var elementToRemove = $(this).attr('toRemove');
@@ -243,7 +192,6 @@ $(document).ready(function() {
                 height: 150,
                 filebrowserUploadUrl: '/ckeditor/img_upload/'
             });
-            
             textarea++;
             edit++;    
         }
@@ -271,18 +219,21 @@ $(document).ready(function() {
         }
         if($("#"+sendBackTo+"").hasClass('explanationRenderer')) {
             $("#"+sendBackTo+"").hide();
-            $("#"+rootBox+"").find('.btnSolution').show();
+            $("#"+rootBox+"").find('.btnSolution').append('<br>').show();
             $('#addQandABox').attr('disabled',false);
+            $("#"+rootBox+"").find('.tagSelector').chosen({
+                width: "30%",
+                no_results_text: "oops! search failed!",
+                allow_single_deselect: true
+            });
             
-            convertSelectToChosen(rootBox);
-            $("#"+rootBox+"").find('.tagSelector').show();
-            $("#"+rootBox+"").find('.btnSubmitTags').attr('disabled', true).show();
-            
-            
+            $("#"+rootBox+"").find('.btnSubmitTags').show();
+            if($("#"+rootBox+"").find('.tagSelector').val() == null) {
+                $("#"+rootBox+"").find('.btnSubmitTags').attr('disabled', true);
+            }
         }
         console.log("questionId : " + questionId + ", " + "elementId : " + elementId + ", " + "textSentBack : " + textSentBack);
         $("#" + editorId + "").remove();
-        
     });
     
     $('body').on('click','.btnCancel', function() {
@@ -299,10 +250,8 @@ $(document).ready(function() {
     
     $('body').on('click','.btnSolution', function() {
         var toggleDiv = $(this).attr('explanationId');
-        //$("#" + toggleDiv + "").show();
         $("#" + toggleDiv + "").slideToggle("fast");
         $(this).text($(this).text() == "view explanation"?"hide explanation":"view explanation");
-        
     });
     
     $('body').on('click','.btnSubmitTags', function() {
@@ -314,25 +263,15 @@ $(document).ready(function() {
         var IDs= [];
         if(newVal!= null) {
             for(var i = 0; i < newVal.length; i++) {
-            
                 $.each(tags, function(key, data) {
-
                     if(data["name"] == newVal[i]) {
                         IDs[i] = data["id"];
                         console.log("id : " + data["id"] + ", " + "name : " + data["name"]);
-
                     }
-
                 });
-
             }
             console.log("ID of selected option(s) : [" + IDs + "]");
-        } else {
-            /*IDs = null;*/
-        }
-        
-        
-        
+        } 
     });
     
     $('body').on('change','.tagSelector', function() {
@@ -343,12 +282,4 @@ $(document).ready(function() {
             $("#"+ boxId +"").find('.btnSubmitTags').attr('disabled',true);
         }
     });
-    
-    /*$('body').on('blur', '.tagSelector', function() {
-        var selectId = $(this).attr('id');
-        
-    });*/
-    
-    
-    
 });
